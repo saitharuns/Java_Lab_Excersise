d5873deeea2d18deb3ee5e15b81d217277d746fa
saitharuns <66780634+saitharuns@users.noreply.github.com>
2022-10-27 13:26:34 +0530

    Add files via upload

---
+++ Assignment2710.java
@@ -0,0 +1,17 @@
+package javaproject;
+
+public class Assignment2710 {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=8,b=2345,c=3,d=5;
+		float e;
+		e=(((b+a)/c)%d)*d;
+		System.out.println(e);
+		
+		
+
+	}
+
+}
---
+++ CheckPassword.java
@@ -0,0 +1,36 @@
+package javaproject;
+import java.util.Scanner;
+
+
+public class CheckPassword {
+	String a,b;// declared a String Variables;	
+	void getpass(String a, String b){ //getting the value;
+		
+		this.a=a;//a in method is assigned to a in class
+		this.b=b;//b in method is assigned to b in class
+	}
+	void checkpass(){ // used to check password;
+		if(this.a.equals(b)) { // it checks the condition
+			System.out.println("The Given Password is Matching");
+		}else {
+			System.out.println("Error Check Again");
+		}
+		
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		CheckPassword cp=new CheckPassword();// 
+		Scanner sc = new Scanner(System.in);
+		String c,d;
+		c=sc.next();
+		d=sc.next();
+		cp.getpass(c, d);
+		cp.checkpass();
+
+	}
+
+}
