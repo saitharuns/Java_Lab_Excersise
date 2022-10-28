885ba36be7327d2e87b1b0cfad67e539ba355620
saitharuns <66780634+saitharuns@users.noreply.github.com>
2022-10-28 10:26:40 +0530

    Update CheckPassword.java

--- CheckPassword.java
+++ CheckPassword.java
@@ -11,9 +11,9 @@ public class CheckPassword {
 	}
 	void checkpass(){ // used to check password;
 		if(this.a.equals(b)) { // it checks the condition
-			System.out.println("The Given Password is Matching");
+			System.out.println("The Given Password is Matching");// if the condition passes then it prints this line.
 		}else {
-			System.out.println("Error Check Again");
+			System.out.println("Error Check Again");// if the condition fails then this statement will be printed.
 		}
 		
 	}
@@ -23,15 +23,15 @@ public class CheckPassword {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		CheckPassword cp=new CheckPassword();// 
-		Scanner sc = new Scanner(System.in);
-		String c,d;
+		CheckPassword cp=new CheckPassword();// object is created for a class
+		Scanner sc = new Scanner(System.in);// object is created for a Scanner class 
+		String c,d;//two strings are created to take the input from the user 
 		System.out.println("Enter Your Password ");
-		c=sc.next();
+		c=sc.next();// takeing first input as password
 		System.out.println("Re-enter Your Password ");
-		d=sc.next();
-		cp.getpass(c, d);
-		cp.checkpass();
+		d=sc.next();// takeing second input as confirm password
+		cp.getpass(c, d);// passing those two input to a getpass parameterized method
+		cp.checkpass();//calling the checkpass method.
 
 	}
 
